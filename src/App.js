6698d242fc361b939873669fb3e3bd9897e74e24
Setbye <jeong.ki.chang@gmail.com>
2021-09-26 13:29:53 +0900

    add jsx sample

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import React from 'react';
+
+function Potato() {
+  return <h1>I like potato</h1>;
+}
+
 function App() {
-  return <div>test</div>;
+  return (
+    <div>
+      <h1>hello</h1>
+      <Potato />
+    </div>
+  );
 }
 
 export default App;
