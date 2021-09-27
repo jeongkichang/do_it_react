2a5241278ac51f76ae98c3685613e9a986638399
Setbye <jeong.ki.chang@gmail.com>
2021-09-27 18:19:04 +0900

    ready create movie app

--- src/App.js
+++ src/App.js
@@ -1,46 +1,21 @@
 import React from 'react';
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
-    console.log('hello');
-  }
-
-  componentDidMount(){
-    console.log('component rendered');
-  }
-
-  componentDidUpdate(){
-    console.log('I just updated');
-  }
-
-  componentWillUnmount(){
-    console.log('Goodbye, cruel world');
-  }
-
   state = {
-    count: 0,
-  }
-
-  add = () => {
-    this.setState({ count: this.state.count + 1 })
+    isLoading: true,
+    movies: [],
   };
 
-  minus = () => {
-    this.setState({ count: this.state.count - 1 })
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
 
   render() {
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>This number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
   }
-
 }
 
 export default App;
